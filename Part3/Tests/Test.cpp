21665348d5d2a2b4099673c5ae8d7ec4878eadb1
kas054 <80067024+kas054@users.noreply.github.com>
2021-10-17 15:23:03 +0300

    Update Test.cpp

--- Part3/Tests/Test.cpp
+++ Part3/Tests/Test.cpp
@@ -2,20 +2,20 @@
 // Created by PC on 23.09.2021.
 //
 #include "gtest/gtest.h"
-#include "../Vector.h"
+#include "../Vector3.h"
 using namespace Prog3;
 TEST(constructor, defaultConstructor)
 {
-    Vector a1;
+    Vector3 a1;
     ASSERT_EQ(0, a1.getCurSize());
 }
 TEST(constructor, initConstructors)
 {
     double numbers[10] = {0};
-    Vector a(4);
+    Vector3 a(4);
     ASSERT_EQ(1, a.getCurSize());
     ASSERT_EQ(4, a.getNum(0));
-    Vector b(10, numbers);
+    Vector3 b(10, numbers);
     ASSERT_EQ(10, b.getCurSize());
     ASSERT_EQ(0, b.getNum(9));
 
@@ -24,37 +24,41 @@ TEST(constructor, initConstructors)
 TEST(methods, add_sub)
 {
     double numbers[10] = {1};
-    Vector a(4);
-    Vector b(10, numbers);
-    Vector *c = a.addition(b);
-    ASSERT_EQ(5, (b.addition(a))->getNum(0));
-    ASSERT_EQ(-3, (b.subtraction(a))->getNum(0));
+    Vector3 a(4);
+    Vector3 b(10, numbers);
+    Vector3 c = a + b;
+    Vector3 d = a - b;
+    ASSERT_EQ(5, (b.addition(a)).getNum(0));
+    ASSERT_EQ(-3, (b.subtraction(a)).getNum(0));
+    ASSERT_EQ(3, d.getNum(0));
+    ASSERT_EQ(5, c.getNum(0));
 }
 TEST(methods, scalar)
 {
     double numbers[10] = {1};
-    Vector a(4);
-    Vector b(10, numbers);
-    ASSERT_EQ(4, b.scalarProduct(a));
+    Vector3 a(4);
+    Vector3 b(10, numbers);
+    ASSERT_EQ(4, b * a);
+    ASSERT_EQ(12, a * 3);
 }
 TEST(methods, normOfVector)
 {
     double numbers[10] = {-100};
-    Vector b(10, numbers);
+    Vector3 b(10, numbers);
     ASSERT_EQ(100, b.normOfVector());
 }
 
 TEST(methods, addElement)
 {
     double numbers[10] = {-100};
-    Vector b(10, numbers);
-    Vector a(4);
+    Vector3 b(10, numbers);
+    Vector3 a(4);
     a.addElement(3);
     ASSERT_EQ(3, a.getNum(1));
 }
 TEST(methods, norma)
 {
     double numbers[10] = {-100};
-    Vector b(10, numbers);
+    Vector3 b(10, numbers);
     ASSERT_EQ(100, b.normOfVector());
 }
